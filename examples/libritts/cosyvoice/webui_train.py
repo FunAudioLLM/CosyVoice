8936377b4b81d36438375caea105a9ddfd8124f6
Ke <justxwy@126.com>
2024-07-23 17:43:41 +0800

    Update webui_train.py
    Add pythonpath env in the script so that we do not need to run `path.sh`.

--- examples/libritts/cosyvoice/webui_train.py
+++ examples/libritts/cosyvoice/webui_train.py
@@ -4,6 +4,8 @@ import gradio as gr
 import subprocess
 from pathlib import Path
 
+os.environ['PYTHONPATH'] = '../../../:../../../third_party/Matcha-TTS:' + os.environ.get('PYTHONPATH', '')
+uttmap = {}
 
 def preprocess(train_input_path, val_input_path, output_path, pre_model_path):
     for state, input_path in zip(['train', 'val'], [train_input_path, val_input_path]):
@@ -39,12 +41,22 @@ def preprocess(train_input_path, val_input_path, output_path, pre_model_path):
 
 
 def refresh_voice(output_path):
-    content = (Path(output_path)/'train'/'temp1'/'utt2spk').read_text()
+    global uttmap
+    content = (Path(output_path)/'train'/'temp1'/'wav.scp').read_text()
     voices = []
     for item in content.split('\n'):
-        voices.append(item.split(' ')[0])
-    return gr.Dropdown(choices=voices)
+        if item:
+            voice, path = item.split(' ')
+            voices.append(voice)
+            uttmap[voice] = path
+    return gr.Dropdown(choices=voices, value=voices[0])
 
+def update_ref_voice(voice):
+    global uttmap
+    if uttmap and voice:
+        audio_path = uttmap[voice]
+        return audio_path
+    
     
 def train(output_path, pre_model_path):
     train_list = os.path.join(output_path, 'train', 'temp2', 'data.list')
@@ -104,9 +116,10 @@ with gr.Blocks() as demo:
     with gr.Tab('Inference'):
         with gr.Row():
             voices = gr.Dropdown(label='Voices')
+            ref_voice = gr.Audio(label='Ref voice')
             refresh = gr.Button('Refresh voices', variant='primary')
-            mode = gr.Dropdown(choices=['sft', 'zero_shot'], label='Mode')
-            epoch = gr.Number(value=8, interactive=True, precision=0, label='Epochs')
+            mode = gr.Dropdown(choices=['sft', 'zero_shot'], label='Mode', value='zero_shot')
+            epoch = gr.Slider(value=8, interactive=True, precision=0, label='Epochs')
         text = gr.Text()
         inference_btn = gr.Button('Inference', variant='primary')
         out_audio = gr.Audio()
@@ -115,5 +128,6 @@ with gr.Blocks() as demo:
     train_btn.click(train, inputs=[output_dir, pretrained_model_path], outputs=status)
     inference_btn.click(inference, inputs=[mode, output_dir, epoch, pretrained_model_path, text, voices], outputs=out_audio)
     refresh.click(refresh_voice, inputs=output_dir, outputs=voices)
+    voices.change(update_ref_voice, inputs=voices, outputs=ref_voice)
 
 demo.launch(server_name='0.0.0.0', enable_queue=True)

77a041712d5ce01fa65af8538eeaaec017725fe8
禾息 <zhoubofan.zbf@antgroup.com>
2024-08-21 16:42:02 +0800

    format test.py

--- test.py
+++ test.py
@@ -5,15 +5,14 @@ import torch
 import logging
 
 cosyvoice = CosyVoice('pretrained_models/CosyVoice-300M-SFT')
-
-print("$$$$$$$$$$$$$$$$$$$$")
+print("finish model initialize")
 
 test_sentences = ['欢迎使用蚂蚁集团语音合成服务', '欢迎使用蚂蚁集团语音合成服务', '欢迎使用蚂蚁集团语音合成服务', '欢迎使用蚂蚁集团语音合成服务', '欢迎使用蚂蚁集团语音合成服务', '欢迎使用蚂蚁集团语音合成服务', '欢迎使用蚂蚁集团语音合成服务', '欢迎使用蚂蚁集团语音合成服务', '欢迎使用蚂蚁集团语音合成服务']
 
 for idx, sent in enumerate(test_sentences):
     output = cosyvoice.inference_sft(sent, '中文女')
     for _, itr in enumerate(output):
-        torchaudio.save(f"zero_shot_3.5s_{idx}.wav", itr['tts_speech'], 22050)
+        torchaudio.save(f"zero_shot_{idx}.wav", itr['tts_speech'], 22050)
     torch.cuda.synchronize()
-    print("-------")
+    print("-------\n")
 

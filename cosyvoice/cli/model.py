bacc053f1fefd9a64c9e1b051e57ab1437847352
szsteven008 <97944818+szsteven008@users.noreply.github.com>
2024-09-01 21:04:50 +0800

    Add files via upload
    修正了对cpu的支持

--- cosyvoice/cli/model.py
+++ cosyvoice/cli/model.py
@@ -54,19 +54,25 @@ class CosyVoiceModel:
     def load(self, llm_model, flow_model, hift_model):
         self.llm.load_state_dict(torch.load(llm_model, map_location=self.device))
         self.llm.to(self.device).eval()
-        self.llm.half()
+        if (self.device == "cuda"): 
+            self.llm.half()
+        else:
+            self.llm.float()
         self.flow.load_state_dict(torch.load(flow_model, map_location=self.device))
         self.flow.to(self.device).eval()
         self.hift.load_state_dict(torch.load(hift_model, map_location=self.device))
         self.hift.to(self.device).eval()
 
     def load_jit(self, llm_text_encoder_model, llm_llm_model):
-        llm_text_encoder = torch.jit.load(llm_text_encoder_model)
+        llm_text_encoder = torch.jit.load(llm_text_encoder_model, map_location=self.device)
         self.llm.text_encoder = llm_text_encoder
-        llm_llm = torch.jit.load(llm_llm_model)
+        llm_llm = torch.jit.load(llm_llm_model, map_location=self.device)
         self.llm.llm = llm_llm
 
     def llm_job(self, text, prompt_text, llm_prompt_speech_token, llm_embedding, uuid):
+        embedding = llm_embedding.to(self.device).float()
+        if (self.device == "cuda"):
+            embedding = llm_embedding.to(self.device).half()
         with self.llm_context:
             for i in self.llm.inference(text=text.to(self.device),
                                                 text_len=torch.tensor([text.shape[1]], dtype=torch.int32).to(self.device),
@@ -74,7 +80,7 @@ class CosyVoiceModel:
                                                 prompt_text_len=torch.tensor([prompt_text.shape[1]], dtype=torch.int32).to(self.device),
                                                 prompt_speech_token=llm_prompt_speech_token.to(self.device),
                                                 prompt_speech_token_len=torch.tensor([llm_prompt_speech_token.shape[1]], dtype=torch.int32).to(self.device),
-                                                embedding=llm_embedding.to(self.device).half(),
+                                                embedding=embedding,
                                                 sampling=25,
                                                 max_token_text_ratio=30,
                                                 min_token_text_ratio=3):
@@ -169,4 +175,5 @@ class CosyVoiceModel:
             self.llm_end_dict.pop(this_uuid)
             self.mel_overlap_dict.pop(this_uuid)
             self.hift_cache_dict.pop(this_uuid)
-        torch.cuda.synchronize()
+        if torch.cuda.is_available():
+            torch.cuda.synchronize()

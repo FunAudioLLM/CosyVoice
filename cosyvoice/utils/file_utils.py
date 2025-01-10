87475ccf41fd4ad88395d5b9f0d2d560397469c7
lyuxiang.lx <lyuxiang.lx@alibaba-inc.com>
2025-01-10 16:43:31 +0800

    fix conflict

--- cosyvoice/utils/file_utils.py
+++ cosyvoice/utils/file_utils.py
@@ -40,7 +40,7 @@ def read_json_lists(list_file):
 
 
 def load_wav(wav, target_sr):
-    speech, sample_rate = torchaudio.load(wav)
+    speech, sample_rate = torchaudio.load(wav, backend='soundfile')
     speech = speech.mean(dim=0, keepdim=True)
     if sample_rate != target_sr:
         assert sample_rate > target_sr, 'wav sample rate {} must be greater than {}'.format(sample_rate, target_sr)

c7b8cb88796e66f92e8c4c224fa9b5ef74c5ce22
minjoonkim01 <0016kmj@gmail.com>
2025-04-03 22:02:11 +0900

    0403 finisih : add some test code

--- cosyvoice/bin/export_trt.sh
+++ cosyvoice/bin/export_trt.sh
@@ -7,10 +7,11 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TRT_DIR/lib:/usr/local/cuda/lib64
 
 # MJKIM README
 # Consider "optShapes" argument, it influence exec time
-
+# load delay (while롸 테스트 한번 해보자) 계속 프로세스가 돌고 있으면 빠르긴 한가? / llm trt로 못돌리나? / fp16저하가 되는데 이게 llm인지 tok2wav
+ 
 # onnx_fp32-> trt_fp16
 # memory issue : reduce maxShape 2x80x6800 -> 2x80x5000
-out_dim=5000
+out_dim=2000
 $TRT_DIR/bin/trtexec --onnx=$MODEL_DIR/flow.decoder.estimator.fp32.onnx \
                      --saveEngine=$MODEL_DIR/flow.decoder.estimator.fp32.mygpu.plan \
                      --minShapes=x:2x80x4,mask:2x1x4,mu:2x80x4,cond:2x80x4 \
@@ -21,11 +22,11 @@ $TRT_DIR/bin/trtexec --onnx=$MODEL_DIR/flow.decoder.estimator.fp32.onnx \
                     #  --optShapes=x:2x80x193,mask:2x1x193,mu:2x80x193,cond:2x80x193 \
 
 # onnx_fp32-> trt_fp32
-$TRT_DIR/bin/trtexec --onnx=$MODEL_DIR/flow.decoder.estimator.fp32.onnx \
-                     --saveEngine=$MODEL_DIR/flow.decoder.estimator.fp16.mygpu.plan \
-                     --fp16 \
-                     --minShapes=x:2x80x4,mask:2x1x4,mu:2x80x4,cond:2x80x4 \
-                     --maxShapes=x:2x80x6800,mask:2x1x6800,mu:2x80x6800,cond:2x80x6800 \
-                     --inputIOFormats=fp16:chw,fp16:chw,fp16:chw,fp16:chw,fp16:chw,fp16:chw \
-                     --outputIOFormats=fp16:chw
-                    #  --optShapes=x:2x80x193,mask:2x1x193,mu:2x80x193,cond:2x80x193
+# $TRT_DIR/bin/trtexec --onnx=$MODEL_DIR/flow.decoder.estimator.fp32.onnx \
+#                      --saveEngine=$MODEL_DIR/flow.decoder.estimator.fp16.mygpu.plan \
+#                      --fp16 \
+#                      --minShapes=x:2x80x4,mask:2x1x4,mu:2x80x4,cond:2x80x4 \
+#                      --maxShapes=x:2x80x6800,mask:2x1x6800,mu:2x80x6800,cond:2x80x6800 \
+#                      --inputIOFormats=fp16:chw,fp16:chw,fp16:chw,fp16:chw,fp16:chw,fp16:chw \
+#                      --outputIOFormats=fp16:chw
+#                     #  --optShapes=x:2x80x193,mask:2x1x193,mu:2x80x193,cond:2x80x193
--- test.py
+++ test.py
@@ -4,28 +4,32 @@ sys.path.append('third_party/Matcha-TTS')
 
 # set test configuration
 DEBUG = True
+use_stream = True
+model_dir='pretrained_models/CosyVoice2-0.5B-trt'
+prompt_wav = './test/assets/voice_kowoon.wav'
+prompt_text = '안녕하세요 저는 고운입니다. 제가 오늘 발표드릴 주제는 인공지능입니다. 잘 부탁 드립니다.'
+output_file ='./test/results/stream_go_{}.wav'
 
 if DEBUG:
     from viztracer import VizTracer
     tracer = VizTracer()
     tracer.start()
 
-from cosyvoice.cli.cosyvoice import CosyVoice, CosyVoice2
+from cosyvoice.cli.cosyvoice import CosyVoice2
 from cosyvoice.utils.file_utils import load_wav
 import torchaudio
 
 
-prompt_speech_16k = load_wav('./test/assets/voice_kowoon.wav', 16000)
-prompt_text = '안녕하세요 저는 고운입니다. 제가 오늘 발표드릴 주제는 인공지능입니다. 잘 부탁 드립니다.'
-output_file ='./test/results/stream_go_{}.wav'
-use_stream = True
-cosyvoice = CosyVoice2('pretrained_models/CosyVoice2-0.5B', load_jit=False, load_trt=True, fp16=True)
+cosyvoice = CosyVoice2(model_dir, load_jit=False, load_trt=True, fp16=True)
+
+prompt_speech_16k = load_wav(prompt_wav, 16000)
 
 def text_generator():
     yield "세상은 매일 진화하고 있습니다."
     yield "AI는 이제 단순한 도구가 아니라,"
     yield "우리 삶의 일부가 되었죠."
     yield "오늘, 그 놀라운 변화를 함께 만나봅니다."
+
 idx = 0
 for i, j in enumerate(cosyvoice.inference_zero_shot(text_generator(), prompt_text, prompt_speech_16k, stream=use_stream)):
     torchaudio.save(output_file.format(i), j['tts_speech'], cosyvoice.sample_rate)

3d64706373298195ad830c16d7b1c20ae813aacf
246p <0016kmj@gmail.com>
2025-04-06 13:00:26 +0900

    Fix tensorrt export script

--- cosyvoice/bin/export_trt.sh
+++ cosyvoice/bin/export_trt.sh
@@ -9,9 +9,9 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TRT_DIR/lib:/usr/local/cuda/lib64
 # Consider "optShapes" argument, it influence exec time
 # load delay (while롸 테스트 한번 해보자) 계속 프로세스가 돌고 있으면 빠르긴 한가? / llm trt로 못돌리나? / fp16저하가 되는데 이게 llm인지 tok2wav
  
-# onnx_fp32-> trt_fp16
+# onnx_fp32-> trt_fp32
 # memory issue : reduce maxShape 2x80x6800 -> 2x80x5000
-out_dim=2000
+out_dim=6800
 $TRT_DIR/bin/trtexec --onnx=$MODEL_DIR/flow.decoder.estimator.fp32.onnx \
                      --saveEngine=$MODEL_DIR/flow.decoder.estimator.fp32.mygpu.plan \
                      --minShapes=x:2x80x4,mask:2x1x4,mu:2x80x4,cond:2x80x4 \
@@ -19,14 +19,14 @@ $TRT_DIR/bin/trtexec --onnx=$MODEL_DIR/flow.decoder.estimator.fp32.onnx \
                      --inputIOFormats=fp32:chw,fp32:chw,fp32:chw,fp32:chw,fp32:chw,fp32:chw \
                      --outputIOFormats=fp32:chw \
                      --verbose
-                    #  --optShapes=x:2x80x193,mask:2x1x193,mu:2x80x193,cond:2x80x193 \
+                     --optShapes=x:2x80x193,mask:2x1x193,mu:2x80x193,cond:2x80x193 \
 
-# onnx_fp32-> trt_fp32
-# $TRT_DIR/bin/trtexec --onnx=$MODEL_DIR/flow.decoder.estimator.fp32.onnx \
-#                      --saveEngine=$MODEL_DIR/flow.decoder.estimator.fp16.mygpu.plan \
-#                      --fp16 \
-#                      --minShapes=x:2x80x4,mask:2x1x4,mu:2x80x4,cond:2x80x4 \
-#                      --maxShapes=x:2x80x6800,mask:2x1x6800,mu:2x80x6800,cond:2x80x6800 \
-#                      --inputIOFormats=fp16:chw,fp16:chw,fp16:chw,fp16:chw,fp16:chw,fp16:chw \
-#                      --outputIOFormats=fp16:chw
-#                     #  --optShapes=x:2x80x193,mask:2x1x193,mu:2x80x193,cond:2x80x193
+# onnx_fp32-> trt_fp16
+$TRT_DIR/bin/trtexec --onnx=$MODEL_DIR/flow.decoder.estimator.fp32.onnx \
+                     --saveEngine=$MODEL_DIR/flow.decoder.estimator.fp16.mygpu.plan \
+                     --fp16 \
+                     --minShapes=x:2x80x4,mask:2x1x4,mu:2x80x4,cond:2x80x4 \
+                     --maxShapes=x:2x80x6800,mask:2x1x6800,mu:2x80x6800,cond:2x80x6800 \
+                     --inputIOFormats=fp16:chw,fp16:chw,fp16:chw,fp16:chw,fp16:chw,fp16:chw \
+                     --outputIOFormats=fp16:chw
+                     --optShapes=x:2x80x193,mask:2x1x193,mu:2x80x193,cond:2x80x193:

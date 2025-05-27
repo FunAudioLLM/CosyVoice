54d21b40f0d981189e430d1fea20008947b1ac08
lyuxiang.lx <lyuxiang.lx@alibaba-inc.com>
2025-05-27 10:40:45 +0800

    update

--- examples/magicdata-read/cosyvoice/path.sh
+++ examples/magicdata-read/cosyvoice/path.sh
@@ -1,3 +1,1 @@
-# NOTE(kan-bayashi): Use UTF-8 in Python to avoid UnicodeDecodeError when LC_ALL=C
-export PYTHONIOENCODING=UTF-8
-export PYTHONPATH=../../../:../../../third_party/Matcha-TTS:$PYTHONPATH
+../../libritts/cosyvoice/path.sh

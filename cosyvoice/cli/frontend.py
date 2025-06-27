ff9694dc2c224a781fb207210cb56d284781c5ea
xxnuo <54252779+xxnuo@users.noreply.github.com>
2025-06-27 14:45:07 +0800

    Fix: Use wetext cache

--- cosyvoice/cli/frontend.py
+++ cosyvoice/cli/frontend.py
@@ -68,7 +68,7 @@ class CosyVoiceFrontEnd:
                 'failed to initialize ttsfrd resource'
             self.frd.set_lang_type('pinyinvg')
         else:
-            self.zh_tn_model = ZhNormalizer(remove_erhua=False, full_to_half=False, overwrite_cache=True)
+            self.zh_tn_model = ZhNormalizer(remove_erhua=False, full_to_half=False, overwrite_cache=False)
             self.en_tn_model = EnNormalizer()
             self.inflect_parser = inflect.engine()
 
